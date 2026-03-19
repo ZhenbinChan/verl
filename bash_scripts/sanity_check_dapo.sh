dd2ba2bfb6ccbe961869645e2c4d9fb8fd9e6132
itsuitsuki <ne_in@foxmail.com>
2026-03-19 23:37:35 +0800

    upd training scripts with good params

--- bash_scripts/sanity_check_dapo.sh
+++ bash_scripts/sanity_check_dapo.sh
@@ -15,9 +15,10 @@ set -x
 # 1. 基础路径设置
 HOME=~
 MODEL_PATH=~/run/models/Qwen2.5-1.5B-Instruct
-DATA_DIR="$HOME/run/work/verl/data/logiqa2k"
+DATA_NAME=logiqa2k
+DATA_DIR="$HOME/run/work/verl/data/${DATA_NAME}"
 export VLLM_ATTENTION_BACKEND=XFORMERS
-ray stop --force
+# ray stop --force
 # ! For some reason, great lakes sets these two env vars
 unset ROCR_VISIBLE_DEVICES
 unset HIP_VISIBLE_DEVICES
@@ -33,8 +34,8 @@ echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 python3 -u -m verl.trainer.main_ppo \
     algorithm.adv_estimator=grpo \
     data.train_files=$DATA_DIR/train.parquet \
-    data.val_files=$DATA_DIR/test.parquet \
-    data.train_batch_size=16 \
+    data.val_files=$DATA_DIR/validation.parquet \
+    data.train_batch_size=4 \
     data.val_batch_size=8 \
     data.max_prompt_length=512 \
     data.max_response_length=2048 \
@@ -44,10 +45,10 @@ python3 -u -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=${MODEL_PATH} \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
-    actor_rollout_ref.actor.ppo_mini_batch_size=16 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=4 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.actor.use_kl_loss=True \
-    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_coef=0.02 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
@@ -57,7 +58,9 @@ python3 -u -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
-    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.temperature=0.8 \
+    actor_rollout_ref.rollout.top_p=0.95 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.fsdp_config.param_offload=False \
     algorithm.use_kl_in_reward=False \
@@ -70,7 +73,7 @@ python3 -u -m verl.trainer.main_ppo \
     trainer.critic_warmup=0 \
     trainer.logger='["console","wandb"]' \
     trainer.project_name='verl-fol' \
-    trainer.experiment_name='qwen1.5b_dapo_sanity_check' \
+    trainer.experiment_name="qwen1.5b_dapo_sanity_check_${DATA_NAME}" \
     trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
