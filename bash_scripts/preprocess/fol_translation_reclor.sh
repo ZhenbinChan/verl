ad3a0357c88e472b377f3ad00f3d34eadcbd25f6
BiNLP <zhenbinchan@gmail.com>
2026-05-07 20:48:36 +0800

    fol scripts

---
+++ bash_scripts/preprocess/fol_translation_reclor.sh
@@ -0,0 +1,6 @@
+python examples/data_preprocess/mcq_preprocess.py \
+    --input_parquet data/reclor/train.parquet \
+    --base_url "http://localhost:4869/v1" \
+    --model "qwen2.5-coder-7b-instruct" \
+    --output_dir data/reclor \
+    --skip_fol_extraction
