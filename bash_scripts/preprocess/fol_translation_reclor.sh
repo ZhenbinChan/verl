385f679283bd93fffd2e68b296249560810d1260
BiNLP <zhenbinchan@gmail.com>
2026-05-07 21:10:08 +0800

    fix: verbose fol pre-processing

--- bash_scripts/preprocess/fol_translation_reclor.sh
+++ bash_scripts/preprocess/fol_translation_reclor.sh
@@ -3,4 +3,4 @@ python examples/data_preprocess/mcq_preprocess.py \
     --base_url "http://localhost:4869/v1" \
     --model "qwen2.5-coder-7b-instruct" \
     --output_dir data/reclor \
-    --skip_fol_extraction
+    --verbose
