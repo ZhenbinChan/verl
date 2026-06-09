ff63c2f7928837dd449de6066924f912e6748984
ZhenbinChan <zhenbinchan@gmail.com>
2026-06-10 00:12:53 +0800

    update: scripts

--- bash_scripts/eval/convert_and_upload.sh
+++ bash_scripts/eval/convert_and_upload.sh
@@ -1,9 +1,9 @@
 base_model_dir="/share/nlp/chenzhenbin/Workspaces/LLMs/Qwen3-8B-Base"
 
-checkpoint_dir="/share/nlp/chenzhenbin/Workspaces/verl/ckpt/verl/qwen3-8b_logiqa_grpo_promptV1_n64_formatp/global_step_115/actor"
+checkpoint_dir="/share/nlp/chenzhenbin/Workspaces/verl/ckpt/verl/qwen3-8b_logiqa_grpo_promptV1_format_v2/global_step_115/actor"
 
 
-output_dir="/share/nlp/chenzhenbin/Workspaces/verl/output_models/qwen3-8b-base_warmup_115"
+output_dir="/share/nlp/chenzhenbin/Workspaces/verl/output_models/qwen3-8b-base_warmup_115_v2"
 
 # python /home/chenzhb/Workspaces/verl/scripts/model_merger.py \
 #     --backend "fsdp" \
@@ -18,7 +18,7 @@ python /share/nlp/chenzhenbin/Workspaces/verl/scripts/model_merger.py \
     --backend "fsdp" \
     --hf_model_path ${base_model_dir} \
     --local_dir ${checkpoint_dir} \
-    --target_dir ${output_dir} \
-    --test \
-    --test_hf_dir ${base_model_dir}
+    --target_dir ${output_dir} 
+#    --test \
+#    --test_hf_dir ${base_model_dir}
 
